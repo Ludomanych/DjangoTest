f33dad14548fd3d3fb79c374c8d06246aff9235b
Iv An <ivan.kovalevych@doontu.ua>
2026-09-22 11:06:14 +0000

    Initial Django application

---
+++ tasks/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    return render(
+        request,
+        'tasks/index.html',
+        {
+            'tasks': tasks
+        }
+    )
